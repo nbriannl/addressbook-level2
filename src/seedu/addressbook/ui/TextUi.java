d278658b8e7e0c34f51bb9eb10ea68a5a073c044
nbriannl <neilbrian.nl@gmail.com>
2017-09-12 15:45:20 +0800

    "add getUserConfirmation() method in UI. implement on Delete Command"

--- src/seedu/addressbook/ui/TextUi.java
+++ src/seedu/addressbook/ui/TextUi.java
@@ -8,10 +8,7 @@ import static seedu.addressbook.common.Messages.MESSAGE_WELCOME;
 
 import java.io.InputStream;
 import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.Scanner;
+import java.util.*;
 
 import seedu.addressbook.commands.CommandResult;
 import seedu.addressbook.data.person.ReadOnlyPerson;
@@ -91,6 +88,27 @@ public class TextUi {
         return fullInputLine;
     }
 
+    /**
+     *
+     */
+    public boolean getUserConfirmation() {
+        String fullInputLine;
+
+        while (true) {
+            out.print(LINE_PREFIX + "Enter Y/N: ");
+            fullInputLine = in.nextLine();
+            // silently consume all ignored lines
+            while (shouldIgnore(fullInputLine)) {
+                fullInputLine = in.nextLine();
+            }
+            if (fullInputLine.trim().equals("Y") || fullInputLine.trim().equals("N")) break;
+            else {
+                showToUser("Invalid Input. Please Enter Y or N");
+            }
+        }
+
+        return fullInputLine.trim().equals("Y");
+    }
 
     public void showWelcomeMessage(String version, String storageFilePath) {
         String storageFileInfo = String.format(MESSAGE_USING_STORAGE_FILE, storageFilePath);
